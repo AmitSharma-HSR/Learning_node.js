094463411fa096f2414e546b39142b0049901a1c
AmitSharma-HSR <146717441+AmitSharma-HSR@users.noreply.github.com>
2023-10-02 18:43:34 +0000

    datatype

---
+++ .devcontainer/besics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict" // Trest all js in newer version
+
+//alert ( 3 + 3 ) ham node js use kar rhe hai to ye nhi chlega
+console.log(3 + 3);
+/* Data types 
+premitive data types 
+number = 2 to power 53
+string = ""
+null = empty value
+undefiened = variale that not defined yet
+bingint = for large number
+boolean = for ture/ false
+sybmle = for uniqueness
+*/
+let name = "amit";
+let age = 23;
+let isLoggedin = true;
+let state;
+
+// Object/
+console.log(typeof null); //object type
+console.log(typeof undefined); // undefined
---
+++ .devcontainer/besics/variable.js
@@ -0,0 +1,16 @@
+const accountId = 97285
+let accountEmail = "newcode@gmail.com"
+var accountPassword ="Amit@123"
+accountCity = "hisar"
+let accountState;
+// accountId = 2 // Const Changing not allowed
+
+accountEmail= "amittest@gmail.com"
+accountPassword = "amit@321"
+accountCity ="haryana"
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.log(accountState);
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState]);
