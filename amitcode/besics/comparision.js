6980606b84ec406fa9a1871d303f3ea1c36966b6
AmitSharma-HSR <146717441+AmitSharma-HSR@users.noreply.github.com>
2023-10-03 19:11:41 +0000

    updated

---
+++ amitcode/besics/comparision.js
@@ -0,0 +1,12 @@
+console.log(2 > 5);
+console.log(5 = 5);
+
+// comparision is always best result in same datatype
+
+/*
+console.log(null > 1);
+console.log(null >= 0);
+console.log(undefined == 0);
+// Stict Chack ===
+console.log("5" === 5);
+this is not a good comparision */
