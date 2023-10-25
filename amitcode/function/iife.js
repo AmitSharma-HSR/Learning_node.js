ab142d95e5539c1946149f05e3d3767c98a58c00
AmitSharma-HSR <146717441+AmitSharma-HSR@users.noreply.github.com>
2023-10-25 19:18:42 +0000

    new

---
+++ amitcode/function/iife.js
@@ -0,0 +1,19 @@
+// imidiate invoked function expression {IIFE}
+
+// (function iife() {                             /// Named IIFE
+//     console.log(`DB CONECTED`)
+// })();
+
+// (() => {
+//     console.log(`DB CONECTED TWO`)
+// })();
+
+
+// ((username) => {
+    // console.log(`DB CONECTED TWO ${username}`)
+// })("amit");
+
+function newone (username) {
+    console.log(`thare is a man ${username}`)
+}
+newone("amit")
